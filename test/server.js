cd50a6c0da579006ce32ce18c423bc044d3d55f3
Victor Fomenko <victorfomenko@me.com>
2017-06-22 00:18:21 +0300

    tests

--- test/server.js
+++ test/server.js
@@ -27,19 +27,92 @@ describe('server tests', () => {
       if (error) return done(error);
 
       const fileContent = fs.readFileSync('files/index.html');
-
       assert.equal(body, fileContent);
       done();
     });
   });
 
   it('post create file', (done) => {
-    const fileContent = fs.readFileSync('files/index.html');
+    const file1 = fs.readFileSync('files/index.html');
+    const options = {
+      url: `http://localhost:3000/${fileName}.html`,
+      body: file1,
+    };
 
-    request('http://localhost:3000/index2.html', (error, response, body) => {
+    request.post(options, (error, response, body) => {
       if (error) return done(error);
 
-      assert.equal(body, fileContent);
+      const file2 = fs.readFileSync(`files/${fileName}.html`);
+      assert.deepEqual(file1, file2);
+      done();
+    });
+  });
+
+  it('post file exists', (done) => {
+    const readStream = fs.createReadStream('files/index.html');
+
+    const req = request.post(`http://localhost:3000/${fileName}.html`, (error, response, body) => {
+      if (error) return done(error);
+      const result = 'File exists';
+
+      assert.equal(body, result);
+      done();
+    });
+
+    readStream
+    .on('error', (err)=>{
+      done(err);
+    })
+    .pipe(req);
+  });
+
+  it('post file size limit', (done) => {
+    const file = fs.readFileSync('files/index.html');
+    const options = {
+      url: `http://localhost:3000/${fileName}.html`,
+      body: file,
+      headers: {
+        'content-length': 1024*1024+1,
+      },
+    };
+
+    request.post(options, (error, response, body) => {
+      if (error) return done(error);
+      const result = 'File is too big!';
+
+      assert.equal(body, result);
+      done();
+    });
+  });
+
+  it('delete existing file', (done) => {
+    request.delete(`http://localhost:3000/${fileName}.html`, (error, response, body) => {
+      if (error) return done(error);
+
+      const result = 'OK';
+      assert.equal(body, result);
+
+      try {
+        fs.readFileSync(`files/${fileName}.html`);
+      } catch (e) {
+        assert.equal(e.code, 'ENOENT');
+      }
+      done();
+    });
+  });
+
+  it('delete NOT existing file', (done) => {
+    request.delete(`http://localhost:3000/${fileName}.html`, (error, response, body) => {
+      if (error) return done(error);
+
+      const result = 'File not found';
+      assert.equal(result, result);
+
+      try {
+        fs.readFileSync(`files/${fileName}.html`);
+      } catch (e) {
+        assert.equal(e.code, 'ENOENT');
+      }
       done();
     });
   });
